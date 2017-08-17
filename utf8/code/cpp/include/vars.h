871b79dc24e8de73dfceb19d261cad45c7f42b23
o-jules <jules3in@gmail.com>
2017-08-17 11:46:21 +0800

    update: utf8, C++, vars.h

--- utf8/code/cpp/include/vars.h
+++ utf8/code/cpp/include/vars.h
@@ -5,23 +5,24 @@
 
 namespace utf8
 {
-static const u8 B10X = 128u;
-static const u8 B110X = 192u;
-static const u8 B1110X = 224u;
-static const u8 B11110X = 240u;
+
+static const u8 B10X = 128u;    // 0b10000000u
+static const u8 B110X = 192u;   // 0b11000000u
+static const u8 B1110X = 224u;  // 0b11100000u
+static const u8 B11110X = 240u; // 0b11110000u
 
 // Unicode range
 static const u64 U1_F = 0x0000ul;
-static const u64 U1_L = 0x007ful;
+static const u64 U1_L = 0x007Ful;
 
 static const u64 U2_F = 0x0080ul;
-static const u64 U2_L = 0x07fful;
+static const u64 U2_L = 0x07FFul;
 
 static const u64 U3_F = 0x0800ul;
-static const u64 U3_L = 0xfffful;
+static const u64 U3_L = 0xFFFFul;
 
 static const u64 U4_F = 0x10000ul;
-static const u64 U4_L = 0x10fffful;
+static const u64 U4_L = 0x10FFFFul;
 } // end of namespace
 
 #endif
